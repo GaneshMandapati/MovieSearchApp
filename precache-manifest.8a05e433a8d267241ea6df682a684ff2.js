a7438a9a1bcf73cf8dbbacb53743a671b7461fa3
Ganesh <mandapatiganesh389@gmail.com>
2020-06-19 19:43:17 +0530

    Updates

---
+++ precache-manifest.8a05e433a8d267241ea6df682a684ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef285cbce905ea5f780afb6c0a67dca6",
+    "url": "/GaneshMadapati/MovieSearchApp/index.html"
+  },
+  {
+    "revision": "26d9e76f92a2ac7f3a54",
+    "url": "/GaneshMadapati/MovieSearchApp/static/css/main.2fad291a.chunk.css"
+  },
+  {
+    "revision": "6584d0ae6c2cd4863f05",
+    "url": "/GaneshMadapati/MovieSearchApp/static/js/2.7bcaccaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GaneshMadapati/MovieSearchApp/static/js/2.7bcaccaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d9e76f92a2ac7f3a54",
+    "url": "/GaneshMadapati/MovieSearchApp/static/js/main.5a5a0a5e.chunk.js"
+  },
+  {
+    "revision": "8636f47a1dfe3622906d",
+    "url": "/GaneshMadapati/MovieSearchApp/static/js/runtime-main.fcba65bf.js"
+  }
+]);
